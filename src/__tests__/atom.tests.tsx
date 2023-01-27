d0e1755b61f0bdd47351a060255e5b949c45ea9b
D0tNet4Fun <D0tNet4Fun@users.noreply.github.com>
2023-01-27 15:39:23 +0200

    Refactored atom tests

--- src/__tests__/atom.tests.tsx
+++ src/__tests__/atom.tests.tsx
@@ -3,23 +3,31 @@ import renderer from "react-test-renderer";
 import { atom } from "../index";
 import React from "react";
 
-test("atom", () => {
+describe("atom", () => {
   const state = atom({
     default: 123,
   });
-
-  let value = 0;
-  const Dummy = () => {
-    [value] = useRecoilState(state);
-    return <></>;
-  };
-  renderer.create(
-    <RecoilRoot>
-      <Dummy />
-    </RecoilRoot>,
-  );
-
-  expect(state.key).toBeDefined();
-  expect(state.key).toBe("atom_0");
-  expect(value).toBe(123);
+  it("should have a key automatically generated", () => {
+    expect(state.key).toBeDefined();
+    expect(state.key).toBe("atom_0");
+  });
+  it("should hold the expected value when passed to useRecoilState", () => {
+    let value = 0;
+    const Dummy = () => {
+      [value] = useRecoilState(state);
+      return <></>;
+    };
+    renderer.create(
+      <RecoilRoot>
+        <Dummy />
+      </RecoilRoot>,
+    );
+    expect(value).toBe(123);
+  });
+  it("should not share the key with another instance", () => {
+    const newState = atom({
+      default: "456",
+    });
+    expect(newState.key).not.toBe(state.key);
+  });
 });
